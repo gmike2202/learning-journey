471494b645a61e678c8b3e2912bbfb62c28f205d
gmike2202 <gmike2202@gmail.com>
2026-07-04 17:59:30 -0400

    completes dockerizing the todo app

--- smolJames/fullbackend/chapter_4/src/routes/authRoutes.js
+++ smolJames/fullbackend/chapter_4/src/routes/authRoutes.js
@@ -1,7 +1,7 @@
 import express from "express";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
-import db from "../db.js";
+import prisma from "../prismaClient.js";
 
 const router = express.Router();
 
@@ -33,11 +33,9 @@ router.post("/register", async (req, res) => {
     });
 
     // create a token
-    const token = jwt.sign(
-      { id: result.lastInsertRowid },
-      process.env.JWT_SECRET,
-      { expiresIn: "24h" },
-    );
+    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+      expiresIn: "24h",
+    });
     res.json({ token });
   } catch (err) {
     console.log(err.message);
@@ -45,7 +43,7 @@ router.post("/register", async (req, res) => {
   }
 });
 
-router.post("/login", (req, res) => {
+router.post("/login", async (req, res) => {
   // we get their email, and we look up the password associated with that email in the database
   // but we get it back and see it's encrypted, which means that we cannot compare it to the one the user just used trying to login
   // so what we can to do, is again, one way encrypt the password the user just entered
@@ -53,8 +51,11 @@ router.post("/login", (req, res) => {
   const { username, password } = req.body;
 
   try {
-    const getUser = db.prepare("SELECT * FROM users WHERE username = ?");
-    const user = getUser.get(username);
+    const user = await prisma.user.findUnique({
+      where: {
+        username: username,
+      },
+    });
 
     // if we cannot find a user associated with that username, return out from the function
     if (!user) {
